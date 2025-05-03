4a60c00153ba35708356c1a182019428a3f5b3bd
Ephraim Gibson <143350957+EphraimGibson@users.noreply.github.com>
2025-05-03 18:42:55 +0000

    implemented the ibservable pattern

--- src/main/java/com/example/Location.java
+++ src/main/java/com/example/Location.java
@@ -1,11 +1,21 @@
 package com.example;
 
-public class Location {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeSupport;
+
+public class Location  {
 
     private String weatherEvent;
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public void setWeatherEvent(String weatherEvent) {
+        support.firePropertyChange("weatherEvent", this.weatherEvent, weatherEvent);
         this.weatherEvent = weatherEvent;
     }
+
+    public void addPropertyChangeListener(WeatherUpdates weatherUpdates){
+        support.addPropertyChangeListener(weatherUpdates);
+    }
+
 
 }
